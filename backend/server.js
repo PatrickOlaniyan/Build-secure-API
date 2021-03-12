5e76a91adf07bb9dbbcf84d04d4c9136e1458104
PatrickOlaniyan <Patrick_Olaniyan@yahoo.co.uk>
2021-03-12 14:28:28 +0000

    created app.js and server file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log ('this is my node app!');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('This is my Arsenal!');
+});
+
+server.listen(process.env.PORT || 3000);
